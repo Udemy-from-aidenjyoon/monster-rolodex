94f94416e8f3d5599e9ba60258f155b3b7dcf88c
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-01-19 18:35:21 -0800

    class component flow
    able to observe a common flow of class components and its lifecycle.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,39 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  // used to initialize
   constructor() {
     super();
 
+    console.log("1");
     this.state = {
-      monsters: [
-        { name: "Jacky" },
-        { name: "Andrei" },
-        { name: "Linda" },
-        { name: "Frank" },
-      ],
+      monsters: [],
     };
   }
 
+  // runs right after the first rendering as thats when component mounts
+  // re-renders when setState gets called
+  componentDidMount() {
+    console.log("3");
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
+  }
+
+  // what to show
   render() {
+    console.log("2");
+
     return (
       <div className="App">
         {this.state.monsters.map((monster, idx) => {
